b44060a72cce13c3c7b14e328ed84967fe03ec2f
apalm112 <apalm112@gmail.com>
2016-03-09 14:06:07 -0800

    Added test_double_linked.py and double_linked.py files.

--- src/double_linked.py
+++ src/double_linked.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """Module for double linked list."""
 
+from linked_list import LinkedList
+
 
 class Node(object):
     """Node class."""
@@ -13,3 +15,28 @@ class Node(object):
 
     def set_next(self, next_node):
         self.next_node = next_node
+
+
+    def set_prev(self, prev_node):
+        self.prev_node = prev_node
+
+
+class DoubleLinkedList(LinkedList):
+    """Class for doube linked list."""
+    def __init__(self, head=None):
+        self.head = head
+
+
+    def insert(self, val):
+        new_node = Node(val)
+        new_node.set_next(self.head)
+        self.head = new_node
+
+
+    def pop(self):
+        return_val = self.head.data
+        self.head = self.head.next_node
+        self.head.prev_node = None
+        return return_val
+
+
