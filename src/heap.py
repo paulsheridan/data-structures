96866bf73cb7cfdfda1888f26be405864802fca1
paulsheridan <paul.sheridan@me.com>
2016-03-16 13:43:11 -0700

    more changes to heap, list no longer requires None at the end

--- src/heap.py
+++ src/heap.py
@@ -5,20 +5,20 @@ class Heap(object):
     '''Create heap using nodes'''
 
     def __init__(self, user_input=None):
-        self.heap = [None]
-        self.size = 0
+        self.heap = []
         try:
-            [self.insert(val) for val in user_input]
+            [self.push(val) for val in user_input]
         except TypeError:
             print('Argument is not iterable.')
 
-    def _sift_up(self, idx):
+    def _sift_up(self):
+        idx = len(self.heap) - 1
         print('**************** sift up')
         start = idx
-        while start//2 > 0:
+        while start > 0:
             print('**** outer while')
             idx = start
-            while idx//2 > 0:
+            while idx > 0:
                 print('idx: ', idx, '  idx//2: ', idx//2)
                 print(self.heap[idx], '**', self.heap[idx//2])
                 if self.heap[idx] > self.heap[idx//2]:
@@ -27,23 +27,22 @@ class Heap(object):
             start = start//2
 
     def _sift_down(self):
-        idx = 1
-        while idx*2 < self.size:
+        idx = 0
+        while idx < len(self.heap) - 1:
             if self.heap[idx] < self.heap[idx*2] or self.heap[idx] < self.heap[(idx*2) + 1]:
                 bigger_child = max(self.heap[idx*2], self.heap[(idx*2) + 1])
                 child_idx = self.heap.index(bigger_child)
                 self.heap[idx], bigger_child = bigger_child, self.heap[idx]
             idx = child_idx
 
-    def insert(self, val):
-        self.size += 1
+    def push(self, val):
         self.heap.append(val)
-        self._sift_up(self.size)
+        self._sift_up()
 
-    def extract(self):
+    def pop(self):
         try:
-            return_val = self.heap[1]
-            del self.heap[1]
+            return_val = self.heap[0]
+            del self.heap[0]
             self.heap.insert(1, self.heap.pop(len(self.heap) - 1))
             self._sift_down()
             return return_val
@@ -51,7 +50,8 @@ class Heap(object):
             print('This heap is empty.')
 
 
-new_heap = Heap([34, 56, 78, 90, 3, 4, 1, 6, 29, 66, 5, 4, 8, 9, 24, 20, 24, 45, 92, 93, 65, 87, 19, 9, 46, 72, 85, 4, 76])
+new_heap = Heap([34, 56, 78, 90, 3, 4, 1, 6, 29, 66, 5, 4, 8, 9, 24, 20,
+                 24, 45, 92, 93, 65, 87, 19, 9, 46, 72, 85, 4, 76, 900])
 print(new_heap.heap)
-new_heap.extract()
+new_heap.pop()
 print(new_heap.heap)
